d40d2f2e2c8bce31f67dbbdc664db6df35689dc4
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-19 19:32:48 +0530

    starting

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-blue-500 text-white text-3xl">
+      HELLO
+    </div>
+  );
+}
